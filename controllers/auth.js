49eaddf1edd4ecc090c9496e77a5f43bfb59502a
Allen K Abraham <allenabraham777@gmail.com>
2021-12-02 19:11:45 +0530

    Enhanced signup workflow and added account verification with otp

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,36 +1,83 @@
 const models = require('../models');
+const utils = require('../utils');
 const sgMail = require('@sendgrid/mail');
 const Redis = require('ioredis');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const redis = new Redis(process.env.REDIS_URL);
 
-
 const signup = async (req, res) => {
   try{
     const {email} = req.body;
     const account = await models.Account.findOne({email})
     if(!!account) {
-      return res.status(400).json({account, message: 'Account already exists'});
+      return res.status(400).json({error: 'Account already exists'});
     } else {
       const newAccount = new models.Account({
         email
       })
-      await newAccount.save();
+      const savedAccount = await newAccount.save();
       const otp = Math.floor(100000 + Math.random() * 900000);
       const token = Math.random().toString(36).substr(2,7);
-      await redis.set(`OTP_VERIFY_TOEKN_${token}`,otp);
+      await redis.set(`SIGNUP_OTP_VERIFY_TOEKN_${email}_${token}`, otp, 'ex', 300);
       const msg = {
         to: email, // Change to your recipient
         from: process.env.FROM_EMAIL, // Change to your verified sender
         subject: 'Please verify your email',
         text: 'and easy to do anywhere, even with Node.js',
-        html: `<section><div>OTP for account verification is </div><h1>${otp}</h1></section>`,
+        html: utils.templates.verifyAccountEmail(otp),
       }
       await sgMail.send(msg);
-      return res.json({token, message: 'OTP send successfully to your email, please use this token and OTP to verify.'});
+      return res.statius(200).json({token, message: 'OTP send successfully to your email, please use this token and OTP to verify. OTP expires in 5 min'});
     }
   }catch(err){
+    return res.status(500);
+  }
+}
+
+const resendVerify = async (req, res) => {
+  try{
+    const {email} = req.body;
+    const account = await models.Account.findOne({email})
+    if(!account) {
+      return res.status(404).json({error: "Account dosen't exists"});
+    } else {
+      const otp = Math.floor(100000 + Math.random() * 900000);
+      const token = Math.random().toString(36).substr(2,7);
+      await redis.set(`SIGNUP_OTP_VERIFY_TOEKN_${email}_${token}`, otp, 'ex', 300);
+      const msg = {
+        to: email, // Change to your recipient
+        from: process.env.FROM_EMAIL, // Change to your verified sender
+        subject: 'Please verify your email',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: utils.templates.verifyAccountEmail(otp),
+      }
+      await sgMail.send(msg);
+      return res.status(200).json({token, message: 'OTP send successfully to your email, please use this token and OTP to verify. OTP expires in 5 min'});
+    }
+  }catch(err){
+    console.error(err);
+    return res.status(500);
+  }
+}
+
+const verifyAccount = async (req, res) => {
+  try {
+    const { email, otp, token } = req.body;
+    const account = await models.Account.findOne({email});
+    if(!account) {
+      return res.status(404).json({error: 'Invalid request'});
+    }
+    const realOtp = await redis.get(`SIGNUP_OTP_VERIFY_TOEKN_${email}_${token}`);
+    if(realOtp && parseInt(realOtp) === otp) {
+      await account.update({isVerified: true});
+      await redis.del(`SIGNUP_OTP_VERIFY_TOEKN_${email}_${token}`);
+      return res.status(200).json({message: 'Account verifed successfully'});
+    } else {
+      return res.status(400).json({error: 'Invalid request'});
+    }
+  } catch(eror){
+    console.error(err);
     return res.status(500);
   }
 }
@@ -41,5 +88,7 @@ const login = async (req, res) => {
 
 module.exports = {
   signup,
-  login
+  login,
+  verifyAccount,
+  resendVerify,
 }
--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -1,8 +1,40 @@
-const express = require('express');
-const controllers = require('../../controllers');
+const express = require("express");
+const Joi = require("joi");
+const controllers = require("../../controllers");
+const middleware = require("../../middleware");
 
 const router = express.Router();
 
-router.post('/', controllers.auth.signup);
+router.post(
+  "/",
+  middleware.validator({
+    payload: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  controllers.auth.signup
+);
+
+router.post(
+  "/resend",
+  middleware.validator({
+    payload: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  controllers.auth.resendVerify
+);
+
+router.post(
+  "/verify",
+  middleware.validator({
+    payload: {
+      email: Joi.string().email().required(),
+      token: Joi.string().required(),
+      otp: Joi.number().required()
+    },
+  }),
+  controllers.auth.verifyAccount
+);
 
 module.exports = router;
